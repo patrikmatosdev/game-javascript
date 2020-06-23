d75e0ebc623e84f01c1de01c1229be6c98031828
Patrik <patrikmatos.tecinfo@gmail.com>
2020-06-23 12:01:02 -0300

    Move Scenary

--- sketch.js
+++ sketch.js
@@ -1,15 +1,45 @@
 let sceneryPicture;
+let picturePerson;
+let scenary;
 
 function preload() {
   sceneryPicture = loadImage("assets/img/cenario/floresta.png");
+  picturePerson = loadImage("assets/img/personagem/correndo.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  scenary = new Scenary(sceneryPicture, 3);
 }
 
 function draw() {
-  background(sceneryPicture);
-  circle(mouseX, mouseY, 200);
-  console.log("draw");
+  scenary.exibe();
+  scenary.move();
+  image(picturePerson, 0, height - 135, 110, 135, 0, 0, 220, 270);
+}
+
+class Scenary {
+  constructor(imagem, velocidade) {
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    (this.x1 = 0), (this.x2 = width);
+  }
+
+  exibe() {
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);
+  }
+
+  move() {
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+
+    if (this.x1 < -width) {
+      this.x1 = width;
+    }
+
+    if (this.x2 < -width) {
+      this.x2 = width;
+    }
+  }
 }
